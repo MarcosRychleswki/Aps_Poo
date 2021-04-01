ece293a266742f79741a1ba0eb8df35a558c4f2f
MarcosRychleswki <vinirychlewski@gmail.com>
2021-04-01 14:56:40 -0300

    classes CC CE CP C finalizada

--- src/Banco/ContaCorrente.java
+++ src/Banco/ContaCorrente.java
@@ -1,13 +1,65 @@
 package Banco;
 
-public class ContaCorrente {
-	
-	
-	private boolean limite;
+public class ContaCorrente extends Conta {
 	
 	
 	
+	protected Double limite = 0.0;
+	
+	
+	
+	public ContaCorrente(Integer numConta, String cliente, Integer cpf) {
+		super(numConta, cliente, cpf);
+	}
 	
 	
 
+	public ContaCorrente(Integer numConta, String cliente, Integer cpf, Double limite) {
+		super(numConta, cliente, cpf);
+		this.limite = limite;
+	}
+
+
+
+	public Double getLimite() {
+		return limite;
+	}
+
+
+
+	public void setLimite(Double limite) {
+		this.limite = limite;
+	}
+	
+
+	
+	public boolean usandoLimite() {
+		
+		if (saldo < 0) {
+			
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	
+public String toString() {
+		
+		
+		return numConta 
+				+ cliente 
+				+ cpf  
+				+ String.format("%.2f", saldo)
+				+ limite;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
